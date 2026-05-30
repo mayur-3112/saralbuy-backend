225efd9a929b4e686c0cfcfe8e0577b8d48dec5d
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-05-30 18:51:58 +0530

    banner related functionality,...

--- src/controllers/admin/banner.controller.js
+++ src/controllers/admin/banner.controller.js
@@ -20,4 +20,61 @@ export const uploadBanner = async (req, res) => {
   }
 };
 
-export const getBanners = async (req, res) => {};
+export const getBanners = async (req, res) => {
+  try {
+    let { page = 1, limit = 10 } = req.query;
+    page = parseInt(page);
+    limit = parseInt(limit);
+    const skip = (page - 1) * limit;
+    const banners = await bannerSchema.find().skip(skip).limit(limit);
+    const totalBanners = await bannerSchema.countDocuments();
+    const totalPages = Math.ceil(totalBanners / limit);
+    ApiResponse.successResponse(res, 200, 'users fetched', {
+      banners,
+      page,
+      totalPages,
+      totalBanners,
+    });
+  } catch (error) {
+    ApiResponse.errorResponse(res, 400, error.message);
+  }
+};
+
+export const BannerDetsById = async (req, res) => {
+  const { bannerId } = req.params;
+  try {
+    const banner = await bannerSchema.findById(bannerId).lean();
+    if (!banner) {
+      return ApiResponse.errorResponse(res, 404, 'Banner not found');
+    }
+    ApiResponse.successResponse(res, 200, 'Banner details fetched', banner);
+  } catch (error) {
+    ApiResponse.errorResponse(res, 400, error.message);
+  }
+};
+
+export const updateBanner = async (req, res) => {
+  const { bannerId } = req.params;
+  const { title, linkUrl,endPoint } = req.body;
+  try {
+    let banner = await bannerSchema.findById(bannerId);
+    if (!banner) {
+      return ApiResponse.errorResponse(res, 404, 'Banner not found');
+    }
+      if (req?.file) {
+      banner.imageUrl = await uploadFile(req.file);
+    }
+  
+
+    const newLinkUrl = banner.domain + endPoint;
+
+    banner.title = title;
+    banner.linkUrl = newLinkUrl;
+    banner.endPoint= endPoint
+    await banner.save();
+    ApiResponse.successResponse(res, 200, 'Banner updated successfully', banner);
+  } catch (error) {
+    console.log(error)
+    ApiResponse.errorResponse(res, 400, error.message);
+  }
+};
--- src/routes/admin/banner.route.js
+++ src/routes/admin/banner.route.js
@@ -1,9 +1,22 @@
 import express from 'express';
-import { getBanners, uploadBanner } from '../../controllers/admin/banner.controller.js';
+import {
+  getBanners,
+  uploadBanner,
+  BannerDetsById,
+  updateBanner
+} from '../../controllers/admin/banner.controller.js';
+
 import auth from '../../middleware/auth.middleware.js';
 import { upload } from '../../utils/multer.js';
+
 const router = express.Router();
 
-router.route('/').all(auth).post(upload.single('image'), uploadBanner).get(getBanners);
+router.route('/')
+  .all(auth)
+  .post(upload.single('image'), uploadBanner)
+  .get(getBanners);
+
+router.put('/:bannerId', auth,upload.single('image'), updateBanner);
+router.get('/get-banner/:bannerId', auth, BannerDetsById);
 
 export default router;

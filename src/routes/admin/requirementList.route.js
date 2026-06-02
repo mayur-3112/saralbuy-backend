785d62d841c784c51f85e05c285499a145482b67
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-06-02 22:06:02 +0530

    lit cng

--- src/routes/admin/requirementList.route.js
+++ src/routes/admin/requirementList.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import auth from '../../middleware/auth.middleware.js';
+import adminAuth from '../../middleware/adminAuth.middleware.js';
 import {
   adminRequirementListing,
   requirementListingById,
 } from '../../controllers/admin/requirementList.controller.js';
 const router = express.Router();
 
-router.get('/get-requirement-listing', auth, adminRequirementListing);
-router.get('/get-requirement-listing-by-id/:id', auth, requirementListingById);
+router.get('/get-requirement-listing', adminAuth, adminRequirementListing);
+router.get('/get-requirement-listing-by-id/:id', adminAuth, requirementListingById);
 export default router;

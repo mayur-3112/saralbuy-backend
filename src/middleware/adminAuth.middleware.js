e631ba862c7df5fe27afa60d5f3c987c3b56d4f2
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-06-02 20:08:12 +0530

    lit cng

--- src/middleware/adminAuth.middleware.js
+++ src/middleware/adminAuth.middleware.js
@@ -13,14 +13,18 @@ const adminAuth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (decoded.role !== 'admin') {
-      return res.status(403).json({
-        success: false,
-        message: 'Admin access required',
-      });
-    }
 
-    req.user = decoded;
+    // if (decoded.role !== 'admin') {
+    //   return res.status(403).json({
+    //     success: false,
+    //     message: 'Admin access required',
+    //   });
+    // }
+
+    req.user = {
+      userId: decoded._id,
+      ...decoded
+    };
     next();
   } catch {
     return res.status(401).json({
